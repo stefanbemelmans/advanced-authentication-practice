0a1216670bdfa6cf3c390682aebbc67652241091
stackbaxter <stackbaxter@yahoo.com>
2017-09-19 12:05:51 -0500

    namechange

--- client/src/SignIn.js
+++ client/src/SignIn.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { FormGroup, ControlLabel, FormControl, Button } from "react-bootstrap";
 
-class SignUp extends Component {
+class SignIn extends Component {
   constructor() {
     super();
 
